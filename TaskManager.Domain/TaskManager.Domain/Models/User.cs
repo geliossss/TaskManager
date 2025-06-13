d59b73d45db5d790f7fafec88b43917b3fd5f642
Mostal4ik <kosta549103@mail.ru>
2025-06-13 17:26:52 +0300

    Переделал структуру. Добавлены проекты Domain и Data, связаны с API. Перенёс базу данных в Data. Модели (User) теперь находятся в Domain

---
+++ TaskManager.Domain/TaskManager.Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }  // Primary Key
+        public string Name { get; set; } = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
