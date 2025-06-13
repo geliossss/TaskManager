799984ae6f9b9ea1fdc9ca5345abff4efc757015
Mostal4ik <kosta549103@mail.ru>
2025-06-13 14:17:27 +0300

    Реализация авторизации

--- TaskManager.Client/TaskManager.Client/Program.cs
+++ TaskManager.Client/TaskManager.Client/Program.cs
@@ -1,10 +1,13 @@
 using TaskManager.Client.Components;
+using TaskManager.Client.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<AuthService>();
 
 var app = builder.Build();
 
---
+++ TaskManager.Client/TaskManager.Client/Services/AuthService.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskManager.Client.Services
+{
+    public class AuthService
+    {
+        public bool IsAuthenticated { get; private set; } = false;
+        public string UserName { get; private set; }
+
+        public void Login(string name)
+        {
+            UserName = name;
+            IsAuthenticated = true;
+        }
+
+        public void Logout()
+        {
+            UserName = null;
+            IsAuthenticated = false;
+        }
+    }
+}
