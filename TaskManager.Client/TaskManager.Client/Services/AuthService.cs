5d6caf48fd3f20baab8f6273542e43a4a60d7eb4
geliossss <163649696+geliossss@users.noreply.github.com>
2025-06-17 16:11:35 +0300

    help

--- TaskManager.Client/TaskManager.Client/Services/AuthService.cs
+++ TaskManager.Client/TaskManager.Client/Services/AuthService.cs
@@ -1,27 +1,30 @@
 ﻿namespace TaskManager.Client.Services
 {
-    public class AuthService
+   public class AuthService
+{
+    public bool IsAuthenticated { get; private set; }
+    public int UserId { get; private set; }
+    public string UserName { get; private set; } = string.Empty;
+    public string Login { get; private set; } = string.Empty;
+    public string Password { get; private set; } = string.Empty;
+
+    public void LoginUser(int userId, string name, string login, string password)
     {
-        public bool IsAuthenticated { get; private set; }
-
-        public string UserName { get; private set; } = string.Empty;
-        public string Login { get; private set; } = string.Empty;
-        public string Password { get; private set; } = string.Empty;
-
-        public void LoginUser(string name, string login, string password)
-        {
-            IsAuthenticated = true;
-            UserName = name;
-            Login = login;
-            Password = password;
-        }
-
-        public void Logout()
-        {
-            IsAuthenticated = false;
-            UserName = string.Empty;
-            Login = string.Empty;
-            Password = string.Empty;
-        }
+        IsAuthenticated = true;
+        UserId = userId;
+        UserName = name;
+        Login = login;
+        Password = password;
     }
+
+    public void Logout()
+    {
+        IsAuthenticated = false;
+        UserId = 0;
+        UserName = string.Empty;
+        Login = string.Empty;
+        Password = string.Empty;
+    }
+}
+
 }
